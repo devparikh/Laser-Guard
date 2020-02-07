907139ea751b7046f293274722f0db1aca34f40c
Dev Parikh <47342287+drp08@users.noreply.github.com>
2020-02-07 12:51:48 -0500

    Add files via upload

---
+++ DataClass.py
@@ -0,0 +1,57 @@
+class customer(self, order, price, personal_information):
+    # create an class to classify the data
+    self.customers = customers
+    self.price = price
+    self.personal_information 
+    customer.order = customer_order
+     
+     credit_card_number = input()
+     company_location = input()
+     credit_card_balance = input()
+     delivery_city = input()
+     delivery_location = input()
+
+    def __init__(self):
+        pass
+
+      # Function to find the price of the customers purchases
+    def price(self, customer, personal_information):
+        # Making a devilevery pay based on distance
+        delivery_pay = {'Brampton': 3.00, 'Missisauga': 4.50, 'Toronto': 10.00}
+        if delivery_city == 'Brampton':
+            final_price = total_price + 3.00
+        elif delivery_city == 'Missisuaga':
+            final_price = total_price + 4.50 
+        else:
+            final_price = total_price + 10.00
+            
+        order_input = str(input("Do you want to make this order")).lower()
+        if order_input == 'yes':
+            print("Order made!")
+        else:
+            print("Order not placed")
+        if order_input =='yes'
+            total_price = 100 * customer.order
+            print("Your price is ${x} this price is not including taxes(for more information on taxes read the section about taxes on the website) plus the shipping fee is {r}".format(x='total_price', r="delivery_city"))
+        
+        # Function that collects all of the customers info given
+    def personal_information(self, customer, company, orders, price, final_cost):
+
+        if credit_card_number == int and len(credit_card_number) == 16:
+            pass
+        else:
+            credit_input = int(input("Can you enter a vaild credit card number! Enter a number"))
+            correct_credit_card = credit_input
+
+        # Making the trascation on the account
+        print("Making transactions...")
+        credit_card_balance = credit_card_balance - final_cost
+        return credit_card_balance
+        print("Your Credit Card balance is ${x}! The database has made a transation from your credit card of ${y}! ".format(x="credit_card_balance", y="final_cost"))
+
+        
+        
+
+        
+        
+        
